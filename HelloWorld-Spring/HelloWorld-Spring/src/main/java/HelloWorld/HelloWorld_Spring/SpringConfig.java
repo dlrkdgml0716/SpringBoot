679f1eba72a7cdaf7420f116c43efdf09af8e158
dlrkdgml0716 <dlrkdgml3737@naver.com>
2025-12-24 22:43:28 +0900

    commit

--- HelloWorld-Spring/HelloWorld-Spring/src/main/java/HelloWorld/HelloWorld_Spring/SpringConfig.java
+++ HelloWorld-Spring/HelloWorld-Spring/src/main/java/HelloWorld/HelloWorld_Spring/SpringConfig.java
@@ -1,20 +1,32 @@
 package HelloWorld.HelloWorld_Spring;
 
+import HelloWorld.HelloWorld_Spring.repository.JdbcMemberRepository;
 import HelloWorld.HelloWorld_Spring.repository.MemberRepository;
 import HelloWorld.HelloWorld_Spring.repository.MemoryMemberRepository;
 import HelloWorld.HelloWorld_Spring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 @Configuration
 
 public class SpringConfig {
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Bean
     public MemberService memberService(){
         return new MemberService(memberRepository());
     }
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+        //return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
