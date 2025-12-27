458823914b6524c1420b46839bf5591b1cd18376
dlrkdgml0716 <dlrkdgml3737@naver.com>
2025-12-27 19:28:58 +0900

    commit

--- HelloWorld-Spring/HelloWorld-Spring/src/main/java/HelloWorld/HelloWorld_Spring/SpringConfig.java
+++ HelloWorld-Spring/HelloWorld-Spring/src/main/java/HelloWorld/HelloWorld_Spring/SpringConfig.java
@@ -1,9 +1,9 @@
 package HelloWorld.HelloWorld_Spring;
 
-import HelloWorld.HelloWorld_Spring.repository.JdbcMemberRepository;
-import HelloWorld.HelloWorld_Spring.repository.MemberRepository;
-import HelloWorld.HelloWorld_Spring.repository.MemoryMemberRepository;
+import HelloWorld.HelloWorld_Spring.aop.TimeTraceAop;
+import HelloWorld.HelloWorld_Spring.repository.*;
 import HelloWorld.HelloWorld_Spring.service.MemberService;
+import jakarta.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,20 +13,34 @@ import javax.sql.DataSource;
 @Configuration
 
 public class SpringConfig {
-    private DataSource dataSource;
+
+    private final MemberRepository memberRepository;
 
     @Autowired
-    public SpringConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
+//    private final EntityManager em;
+//
+//    public SpringConfig(EntityManager em) {
+//        this.em = em;
+//    }
+
+//    private DataSource dataSource;
+//    @Autowired
+//    public SpringConfig(DataSource dataSource) {
+//        this.dataSource = dataSource;
+//    }
 
     @Bean
     public MemberService memberService(){
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
-    @Bean
-    public MemberRepository memberRepository(){
-        //return new MemoryMemberRepository();
-        return new JdbcMemberRepository(dataSource);
-    }
+//    @Bean
+//    public MemberRepository memberRepository(){
+//        return new MemoryMemberRepository();
+//        return new JdbcMemberRepository(dataSource);
+//        return new JdbcTemplateMemberRepository(dataSource);
+//        return new JpaMemberRepository(em);
+//    }
 }
